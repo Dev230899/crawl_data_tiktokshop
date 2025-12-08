593a7b2f3fbfa2c6d48fbbf8983170a8ef023c7c
Dev230899 <hoangvanphuocdev@gmail.com>
2025-12-08 15:18:55 +0700

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -6,17 +6,17 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     # redis connection
     REDIS_URL: str = (
-        "redis://:7tS1ZosMDzTWBWqK@redis-master.redis.svc.cluster.local:6379/0"
+        ""
     )
 
-    KAFKA_BOOTSTRAP_SERVERS: str = "kafka.crawl.tmtco.org:9092"
+    KAFKA_BOOTSTRAP_SERVERS: str = ""
 
     # mongo connection
     DB_DATA_LAKE: str = (
-        "mongodb+srv://dev-crawler:2OPKYWjHWtZWGR@rscrawl.mdb.crawl.tmtco.org/crawlerDL?authMechanism=SCRAM-SHA-256&authSource=crawlerDL&ssl=false"
+        ""
     )
     DB_DATA_WARE_HOUSE: str = (
-        "mongodb+srv://dev-crawler:2OPKYWjHWtZWGR@rscrawl.mdb.crawl.tmtco.org/crawlerDW?authMechanism=SCRAM-SHA-256&authSource=crawlerDW&ssl=false"
+        ""
     )
     # minio connection, lưu trữ file, ảnh
     MINIO_URL: str = "localhost:9000"
